1210bbf503f8d199a926a73d0006bc852278dace
Rio Carl H. De la Cruz <115770744+Eyunoeh@users.noreply.github.com>
2022-12-05 13:13:02 +0800

    Add files via upload

---
+++ BookingSystem.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class Airplane{
+    protected HashMap <String,Integer> planes = new HashMap<>();
+    public Airplane(){
+        planes.put("Air_1",15);
+        planes.put("Air_2",15);
+        planes.put("Air_3",15);
+        planes.put("Air_4",15);
+    }
+    public void PlaneList(){
+        SortedMap<String, Integer> sorted_map = new TreeMap<>(planes);
+        for (String plane: sorted_map.keySet()) {
+            if (sorted_map.get(plane)!= 0){
+                System.out.println(plane+" : "+ planes.get(plane) +" Available seats");
+            }
+            else {
+                System.out.println(plane+" : Not Available");
+            }
+        }
+    }
+}
+class Booking extends Airplane{
+    public void book(HashMap<String,Integer>books) {
+        for (String plane_name : books.keySet()) {
+            int available_seat = planes.get(plane_name);
+            if(books.get(plane_name) > 0) {
+                if (books.get(plane_name) <= available_seat) {
+                    int seat_capacity = available_seat - books.get(plane_name);
+                    planes.put(plane_name, seat_capacity);
+                    System.out.println("Booked\n");
+                }
+                else {
+                    System.out.println("Cant accommodate that passenger try again\n");
+                }
+            }
+            else{
+                System.out.println("Must be greater than 0!");
+            }
+        }
+    }
+    public int Seat_total(){
+        int total = 0;
+        for(String items : planes.keySet()){
+            total+= planes.get(items);
+
+        }
+        return total;
+    }
+}
+public class BookingSystem {
+    public static void main(String[] args) {
+        Booking book = new Booking();
+        Scanner sc = new Scanner(System.in);
+        HashMap <String,Integer> bookingHash =new HashMap<>();
+
+        System.out.println("Welcome to CVSU Flight Booking");
+        System.out.println("\nHere are the list of Available Airlines today");
+        book.PlaneList();
+        int total_books = book.Seat_total();
+        while (total_books!=0){
+            System.out.println("""
+     
+                    Type (book) to book flight
+                    Type (flights) to see available flights""");
+            String flight = sc.nextLine().toLowerCase();
+            if (flight.equals("book")) {
+                System.out.print("Please select Airplane to book: ");
+                String airplane = sc.nextLine();
+
+                System.out.print("How many passenger: ");
+                int book_number = sc.nextInt();
+                sc.nextLine();
+
+                bookingHash.put(airplane, book_number);
+                book.book(bookingHash);
+            }
+            else if (flight.equals("flights")) {
+                book.PlaneList();
+            }
+            total_books = book.Seat_total();
+            bookingHash.clear();
+
+        }
+    }
+}
